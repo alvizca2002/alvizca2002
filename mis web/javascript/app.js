4118d16d5d0f5e1250301f81af7632030a7eddd8
Alberto Vizcaino Gonzalez <alvizca2002@gmail.com>
2022-03-30 23:49:03 +0200

    comit

---
+++ mis web/javascript/app.js
@@ -0,0 +1,6 @@
+alert('bienvenido');
+var minumero= 2;
+minumero=minumero*(25+3);
+minumero2=11%3;
+alert(minumero);
+alert(minumero2);
---
+++ mis web/javascript/app2.js
@@ -0,0 +1,7 @@
+var mitexto="la felicidad no es hacer lo que uno quiere\r\n";
+mitexto=mitexto+"si no querer lo que uno hace.";
+alert(mitexto);
+var valido=true;
+alert(valido);
+var miarray=["rojo","verde","azul"];
+alert(miarray[0]);
